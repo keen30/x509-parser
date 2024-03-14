d9b435f64f9be8678718c947717dc4b55c148ef4
keen30 <zapa3000@gmail.com>
2024-03-14 20:54:21 +0700

    update

--- include/x509_parser.c
+++ include/x509_parser.c
@@ -17,17 +17,82 @@ void x509_parse_init( X509_Cert_t *cert )
 {
     Certificate = cert;
 }
-void x509_parse( u1 read );
-void x509_parse_tag( u1 read );
-void x509_parse_length( u1 read );
+
+void x509_parse( void )
+{
+    u1 dataPtr_idx = 0;
+    Certificate->readPtr = Certificate->data;
+    while ( Certificate->length > dataPtr_idx )
+    {
+        u1 data = *( Certificate->readPtr );
+        x509_parse_tag( data );
+        x509_parse_length( data );
+        x509_parse_content( data );
+
+        if( PARSE_READCONTENT_COMPLETE == tlvInfo.readContentState )
+        {
+            x509_parse_attr_version();
+            x509_parse_attr_serial();
+            x509_parse_attr_sigAlgo_oid();
+            x509_parse_attr_issuer_oid_cn();
+            x509_parse_attr_issuer_cn();
+            x509_parse_attr_validityNotBefore();
+            x509_parse_attr_validityNotAfter();
+            x509_parse_attr_subject_oid_cn();
+            x509_parse_attr_subject_cn();
+            x509_parse_attr_subjPubKey_algo_oid();
+            x509_parse_attr_subjPubKey_key();
+            x509_parse_attr_subjPubKey_keyExp();
+            x509_parse_attr_sigAlgo_oid();
+            x509_parse_attr_sigInfo_algo_oid();
+            x509_parse_attr_sigInfo_value();
+        }
+
+        Certificate->readPtr++;
+    }
+
+}
+
+void x509_parse_tag( u1 read )
+{
+    if( PARSE_TLV_TAG_STATE == parse_tlv_state )
+    {
+        tlvInfo.class = ( read&CLASS_BITS_MASK )>>CLASS_BITS_POS;
+        tlvInfo.encoding_form = ( read&ENCODING_FORM_BIT_MASK )>>ENCODING_FORM_BIT_POS;
+        tlvInfo.tag = read&TAG_NUMBER_MASK;
+        tlvInfo.attrBuffer = NULL;
+        tlvInfo.length = 0;
+        tlvInfo.readContentState = PARSE_READCONTENT_IDLE;
+        parse_tlv_state = PARSE_TLV_LENGTH_STATE;
+    }
+}
+
+void x509_parse_length( u1 read )
+{
+    if(PARSE_TLV_LENGTH_STATE)
+    {
+        u1 isLengthExtended = ( read&LENGTH_EXTENDED_BIT_MASK )>>LENGTH_EXTENDED_BIT_POS;
+        if( LENGTH_EXTENDED_TRUE == isLengthExtended )
+        {
+            
+        }
+    }
+
+}
+
+
 void x509_parse_content( u1 read );
-void x509_parse_attr_version( u1 read );
-void x509_parse_attr_serial( u1 read );
-void x509_parse_attr_sigAlgo( u1 read );
-void x509_parse_attr_issuer( u1 read );
-void x509_parse_attr_validity( u1 read );
-void x509_parse_attr_subject( u1 read );
-void x509_parse_attr_subjPubKey_algo( u1 read );
-void x509_parse_attr_subjPubKey_key( u1 read );
-void x509_parse_attr_sigInfo_algo( u1 read );
-void x509_parse_attr_sigInfo_value( u1 read );
+void x509_parse_attr_version( void );
+void x509_parse_attr_serial( void );
+void x509_parse_attr_sigAlgo_oid( void );
+void x509_parse_attr_issuer_oid_cn( void );
+void x509_parse_attr_issuer_cn( void );
+void x509_parse_attr_validityNotBefore( void );
+void x509_parse_attr_validityNotAfter( void );
+void x509_parse_attr_subject_oid_cn( void );
+void x509_parse_attr_subject_cn( void );
+void x509_parse_attr_subjPubKey_algo_oid( void );
+void x509_parse_attr_subjPubKey_key( void );
+void x509_parse_attr_subjPubKey_keyExp( void );
+void x509_parse_attr_sigInfo_algo_oid( void );
+void x509_parse_attr_sigInfo_value( void );
--- include/x509_parser.h
+++ include/x509_parser.h
@@ -14,7 +14,9 @@
 
 #include "base_types.h"
 
-#define MAX_CERTIFICATES    1
+#define MAX_CERTIFICATES                1
+
+#define EXTENDED_LENGTH_BUFFER_SIZE     2 /* This means 2 bytes unsigned integer*/
 
 /**
  * @brief DER Encoding of ASN.1 Types
@@ -39,18 +41,20 @@
 #define TAG_SEQUENCE                (u1)0x30
 #define TAG_SET                     (u1)0x31
 #define TAG_CHOICE                  (u1)0x82
+#define TAG_IMPLICIT_PRIMITIVE_MASK     (u1)0x80
+#define TAG_IMPLICIT_CONSTRUCTIVE_MASK  (u1)0xA0
 
-#define TAG_IMPLICIT_PRIMITIVE      (u1)0x80
-#define TAG_IMPLICIT_CONSTRUCTIVE   (u1)0xA0
+
+#define BYTE_MASK_1BIT              (u1)0b1
+#define BYTE_MASK_2BITS             (u1)0b11
+#define BYTE_MASK_5BITS             (u1)0b11111
+
+#define TAG_NUMBER_MASK             BYTE_MASK_5BITS
 
 #define CLASS_UNIVERSAL             (u1)0b00
 #define CLASS_APPLICATION           (u1)0b01
 #define CLASS_CONTEXT_SPECIFIC      (u1)0b10
 #define CLASS_PRIVATE               (u1)0b11
-
-#define BYTE_MASK_1BIT              (u1)0b1
-#define BYTE_MASK_2BITS             (u1)0b11
-#define BYTE_MASK_5BITS             (u1)0b11111
 
 #define ENCODING_FORM_BIT_POS       5
 #define ENCODING_FORM_BIT_MASK      (u1)( BYTE_MASK_1BIT<<ENCODING_FORM_BIT_POS )
@@ -61,15 +65,25 @@
 #define LENGTH_EXTENDED_BIT_POS     7
 #define LENGTH_EXTENDED_BIT_MASK    (u1)( BYTE_MASK_1BIT<<LENGTH_EXTENDED_BIT_POS )
 
-#define PARSE_SUCCESS                       1
-#define PARSE_FAIL                          0
+#define LENGTH_EXTENDED_TRUE        1
 
-#define PARSE_FORMAT_ERR                    1
-#define PARSE_SERIAL_ERR                    2
-#define PARSE_ISSUER_ERR                    3
-#define PARSE_VALIDITY_ERR                  4
-#define PARSE_SUBJECT_ERR                   5
-#define PARSE_SIGNATURE_ERR                 6
+#define PARSE_SUCCESS                       (u1)1
+#define PARSE_FAIL                          (u1)0
+
+#define PARSE_FORMAT_ERR                    (u1)1
+#define PARSE_SERIAL_ERR                    (u1)2
+#define PARSE_ISSUER_ERR                    (u1)3
+#define PARSE_VALIDITY_ERR                  (u1)4
+#define PARSE_SUBJECT_ERR                   (u1)5
+#define PARSE_SIGNATURE_ERR                 (u1)6
+
+#define PARSE_READCONTENT_IDLE              (u1)0
+#define PARSE_READCONTENT_READING           (u1)1
+#define PARSE_READCONTENT_COMPLETE          (u1)2
+
+#define PARSE_READLENGTH_IDLE               (u1)0
+#define PARSE_READLENGTH_EXTENDED           (u1)1
+#define PARSE_READLENGTH_COMPLETE           (u1)2
 
 typedef enum {
     PARSE_TLV_TAG_STATE,
@@ -79,6 +93,9 @@ typedef enum {
 } PARSE_TLV_STATE;
 
 typedef enum {
+    PARSE_ATTR_CERT_STATE ,  
+    PARSE_ATTR_TBSCERT_STATE ,  
+    PARSE_ATTR_CONTEXT_SPECIFIC_STATE ,     
     PARSE_ATTR_VERSION_STATE ,           
     PARSE_ATTR_SERIAL_STATE,             
     PARSE_ATTR_SIGALGO_STATE ,           
@@ -88,15 +105,19 @@ typedef enum {
     PARSE_ATTR_SUBJECT_STATE ,          
     PARSE_ATTR_SUBJPUBKEY_ALGO_STATE,    
     PARSE_ATTR_SUBJPUBKEY_KEY_STATE,    
+    PARSE_ATTR_SIGINFO_STATE ,   
     PARSE_ATTR_SIGINFO_ALGO_STATE ,     
     PARSE_ATTR_SIGINFO_VALUE_STATE  
 } PARSE_ATTR_STATE; 
 
 typedef struct {
+    u1 class;
     u1 encoding_form;
     u1 tag;
     u2 length;
+    u1 readLengthState;
     u1 *attrBuffer;
+    u1 readContentState;
 } tlv_info_t;
 
 
@@ -105,6 +126,28 @@ typedef struct {
     u1 *readPtr;
     u1 *data;
 } X509_Cert_t;
+
+typedef struct {
+    u1 version;
+    u1 sn_length;
+    u1 *sn;
+    u1 sigAlgo_length;
+    u1 *sigAlgo;
+    u1 issuer_cn_length;
+    u1 *issuer_cn;
+    u1 validityNotBefore_length;
+    u1 *validityNotBefore;
+    u1 validityNotAfter_length;
+    u1 *validityNotAfter;
+    u1 subject_cn_length;
+    u1 *subject_cn;
+    u1 publicKey_length;
+    u1 *publicKey;
+    u1 publicKeyExp_length;
+    u1 *publicKeyExp;
+    u1 signatureInfoValue_length;
+    u1 *signatureInfoValue;
+} X509_Cert_Attributes_t;
 
 tlv_info_t tlvInfo;
 
@@ -118,20 +161,25 @@ u1 parse_attribute_state;
 
 void x509_load( X509_Cert_t *cert );
 void x509_parse_init( X509_Cert_t *cert);
-void x509_parse( u1 read );
+void x509_parse( void );
 void x509_parse_tag( u1 read );
 void x509_parse_length( u1 read );
 void x509_parse_content( u1 read );
-void x509_parse_attr_version( u1 read );
-void x509_parse_attr_serial( u1 read );
-void x509_parse_attr_sigAlgo( u1 read );
-void x509_parse_attr_issuer( u1 read );
-void x509_parse_attr_validity( u1 read );
-void x509_parse_attr_subject( u1 read );
-void x509_parse_attr_subjPubKey_algo( u1 read );
-void x509_parse_attr_subjPubKey_key( u1 read );
-void x509_parse_attr_sigInfo_algo( u1 read );
-void x509_parse_attr_sigInfo_value( u1 read );
+void x509_parse_attr_version( void );
+void x509_parse_attr_serial( void );
+void x509_parse_attr_sigAlgo_oid( void );
+void x509_parse_attr_issuer_oid_cn( void );
+void x509_parse_attr_issuer_cn( void );
+void x509_parse_attr_validityNotBefore( void );
+void x509_parse_attr_validityNotAfter( void );
+void x509_parse_attr_subject_oid_cn( void );
+void x509_parse_attr_subject_cn( void );
+void x509_parse_attr_subjPubKey_algo_oid( void );
+void x509_parse_attr_subjPubKey_key( void );
+void x509_parse_attr_subjPubKey_keyExp( void );
+void x509_parse_attr_sigInfo_algo_oid( void );
+void x509_parse_attr_sigInfo_value( void );
+
 
 
 #endif
